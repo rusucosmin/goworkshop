5581a4c8ca1a694b0cf251bad2095cac285ff12f
rusucosmin <cr.rusucosmin@gmail.com>
2018-03-08 18:06:08 +0200

    Added webserver

--- web/webserver.go
+++ web/webserver.go
@@ -6,19 +6,24 @@ import (
 	"fmt"
 	"encoding/json"
 	"github.com/rusucosmin/goworkshop/model"
+	"github.com/gorilla/mux"
 )
 
 const API_PORT_NAME = "API_PORT"
 const API_PORT_VALUE = "8000"
 
 func StartServer() {
-	http.HandleFunc("/ping", pingHandler)
-	http.HandleFunc("/books", booksHandler)
-	http.HandleFunc("/authors", authorsHandler)
+	router := mux.NewRouter()
+	router.HandleFunc("/ping", pingHandler).Methods("GET")
+	router.HandleFunc("/books", getAllBooksHandler).Methods("GET")
+	router.HandleFunc("/authors", getAllAuthorsHandler).Methods("GET")
+	router.HandleFunc("/authors/{uuid}", getAuthorByUUIDHandler).Methods("GET")
+	router.HandleFunc("/books/{uuid}", getBookByUUIDHandler).Methods("GET")
 	var port = getPort()
 	fmt.Println("+-------------------------------+")
 	fmt.Printf("| Starting sever on port: %s\t|\n", port)
 	fmt.Println("+-------------------------------+")
+	http.Handle("/", router)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		panic(err)
 	}
@@ -29,7 +34,7 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "{\"ping\":\"pong\"}")
 }
 
-func booksHandler(w http.ResponseWriter, r *http.Request) {
+func getAllBooksHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	serializedContent, err := json.Marshal(model.Books)
 	if err != nil {
@@ -38,13 +43,43 @@ func booksHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, string(serializedContent))
 }
 
-func authorsHandler(w http.ResponseWriter, r *http.Request) {
+func getAllAuthorsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	serializedContent, err := json.Marshal(model.Authors)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(w, string(serializedContent))
+}
+
+func getBookByUUIDHandler(w http.ResponseWriter, r *http.Request) {
+	UUID := mux.Vars(r)["uuid"]
+	w.Header().Set("Content-Type", "application/json")
+	for _, book := range model.Books {
+		if book.UUID == UUID {
+			serializedContent, err := json.Marshal(book)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Fprintln(w, string(serializedContent))
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func getAuthorByUUIDHandler(w http.ResponseWriter, r *http.Request) {
+	UUID := mux.Vars(r)["uuid"]
+	w.Header().Set("Content-Type", "application/json")
+	for _, author := range model.Authors {
+		if author.UUID == UUID {
+			serializedContent, err := json.Marshal(author)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Fprintln(w, string(serializedContent))
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func getPort() string {
